b20b0d4957a064a05cce59b11efe92ca6e593051
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-06-13 16:17:09 +0200

    :alembic: 🐐

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { modalPlugin } from "./lib";
+import { createVfm } from "vue-final-modal";
 
 const app = createApp(App);
-app.use(modalPlugin).mount("#app");
+app.use(modalPlugin, createVfm()).mount("#app");
